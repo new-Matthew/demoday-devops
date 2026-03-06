d964ce416f9363c367f3b43ca77979578e07f76a
matheus <matheusrodrig16@gmail.com>
2026-03-05 22:18:22 -0300

    feat: infrastructure as code and ci/cd alignment

--- app.py
+++ app.py
@@ -1,25 +1,12 @@
-import time
-import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    count = get_hit_count()
-    return f'Demo Day DevOps: Fui visualizado {count} vezes.\n'
+    return "Demo Day DevOps: Aplicação rodando de forma independente em container.\n"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
